198b81cbb9e9810d17b77e4f12a3846c1d17771f
Sohell <sohelindiego@gmail.com>
2025-07-10 19:18:15 -0500

    * Slider [Main] * Signup [Sin interacción]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 }
--- app/src/main/java/com/example/appmov_prod/MainActivity.kt
+++ app/src/main/java/com/example/appmov_prod/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.example.appmov_prod
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager2
+    private lateinit var handler: Handler
+    private val delay = 5000L
+    private var page = 0
+
+    private val items = listOf(
+        OnboardingItem(R.drawable.slider, "Recycle from Home", "Find the nearest recycling drop-off points with real-time updates."),
+        OnboardingItem(R.drawable.slider2, "Nearby Recycling Stations", "Find the nearest recycling drop-off points with real-time updates.")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        viewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = OnboardingAdapter(items)
+
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        TabLayoutMediator(tabLayout, viewPager) { _, _ -> }.attach()
+
+        handler = Handler(Looper.getMainLooper())
+        autoSlide()
+    }
+
+    private fun autoSlide() {
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                if (page == items.size) page = 0
+                viewPager.setCurrentItem(page++, true)
+                handler.postDelayed(this, delay)
+            }
+        }, delay)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 }
